2b868d2dd10a325082aaa5fd111390fb25b8fb0f
Wepower383 <venomas56@gmail.com>
2025-07-24 10:53:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,30 +3,37 @@ function enterSimulation() {
   document.getElementById("simulator").style.display = "block";
 }
 
-let voltage = 0;
+let componentId = 0;
 let resistance = 0;
+const voltage = 5;
 
 function addComponent(type) {
-  const workspace = document.getElementById("workspace");
-  const component = document.createElement("div");
-  component.className = "component";
+  const area = document.getElementById("circuitArea");
+  const div = document.createElement("div");
+  div.classList.add("component");
+  div.setAttribute("draggable", true);
+  div.setAttribute("data-tooltip", `${capitalize(type)} (double-click to remove)`);
+  div.innerText = type === 'battery' ? "🔋 Battery (+5V)" : 
+                   type === 'resistor' ? "🌀 Resistor" : 
+                   "🧲 Coil";
 
-  if (type === "battery") {
-    voltage += 5;
-    component.innerText = "🔋 Battery (+5V)";
-  } else if (type === "resistor") {
-    resistance += 10;
-    component.innerText = "🔧 Resistor (10Ω)";
-  } else if (type === "coil") {
-    resistance += 5;
-    component.innerText = "🌀 Coil (5Ω)";
-  }
+  div.style.left = `${50 + Math.random() * 300}px`;
+  div.style.top = `${50 + Math.random() * 100}px`;
 
-  workspace.appendChild(component);
-  updateOutput();
+  div.ondblclick = () => {
+    area.removeChild(div);
+    if (type === 'resistor') resistance -= 10;
+    updateReadings();
+  };
+
+  div.onmousedown = dragMouseDown;
+  area.appendChild(div);
+
+  if (type === 'resistor') resistance += 10;
+  updateReadings();
 }
 
-function updateOutput() {
+function updateReadings() {
   const current = resistance > 0 ? (voltage / resistance).toFixed(2) : 0;
   document.getElementById("voltage").innerText = voltage;
   document.getElementById("resistance").innerText = resistance;
@@ -34,8 +41,36 @@ function updateOutput() {
 }
 
 function resetSimulator() {
-  document.getElementById("workspace").innerHTML = "";
-  voltage = 0;
+  document.getElementById("circuitArea").innerHTML = "";
   resistance = 0;
-  updateOutput();
+  updateReadings();
+}
+
+function dragMouseDown(e) {
+  e.preventDefault();
+  const elmnt = e.target;
+  let pos3 = e.clientX;
+  let pos4 = e.clientY;
+
+  function elementDrag(e) {
+    e.preventDefault();
+    const pos1 = pos3 - e.clientX;
+    const pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+
+  document.onmouseup = closeDragElement;
+  document.onmousemove = elementDrag;
+}
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
