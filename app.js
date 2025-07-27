c0716c98ad01d1c57acbbdc2fcd462d2457893fd
Wepower383 <venomas56@gmail.com>
2025-07-27 19:50:43 -0400

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-function init() {
-  alert('Simulation Initialized: Layer 7 Ready');
-  const simulator = document.getElementById('simulator');
-  simulator.innerHTML = '<p>Components ready: Battery, Resistor, Diode, Capacitor, Coil</p>';
-}
