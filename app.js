fa0e0b798d85711494dc0b0f5f2f0a2fae8b0f5a
Wepower383 <venomas56@gmail.com>
2025-07-27 18:32:07 -0400

    Delete app.js

--- app.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('batteryBtn').onclick = () => alert('Battery added');
-document.getElementById('resistorBtn').onclick = () => alert('Resistor added');
-document.getElementById('groundBtn').onclick = () => alert('Ground added');
