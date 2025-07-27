b2da7bd27123ba3d28f91c279652526fa0e4e5dd
Wepower383 <venomas56@gmail.com>
2025-07-27 19:22:25 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+function init() {
+  alert('Simulation Initialized: Layer 7 Ready');
+  const simulator = document.getElementById('simulator');
+  simulator.innerHTML = '<p>Components ready: Battery, Resistor, Diode, Capacitor, Coil</p>';
+}
